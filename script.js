d2319331f04305f647a46b2c4b642f36e20489ce
Mohammad Danish <30754502+4hydra4@users.noreply.github.com>
2023-05-12 12:51:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -119,9 +119,7 @@ const displayMovements = function (account, sort) {
 
     const html = `
       <div class="movements__row">
-        <div class="movements__type movements__type--${type}">${
-      i + 1
-    } ${type}</div>
+        <div class="movements__type movements__type--${type}">${type}</div>
         <div class="movements__date">${displayDate}</div>
         <div class="movements__value">${formattedMov}</div>
       </div>
